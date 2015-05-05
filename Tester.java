5bce1e836708a14aaa253a2c6d92c37c4cc52d8e
ekinoguz <ekinoguz@gmail.com>
2015-05-04 23:51:22 -0700

    Adding new tests to Tester.java

--- Tester.java
+++ Tester.java
@@ -26,7 +26,76 @@ public class Tester {
       // No exception should occur.
     }
 
-    // Test SJFScheduler
+    // Public test 3
+    try {
+      input = "tests/fcfs3.input";
+      output = "tests/fcfs3.output";
+      scheduler = new FCFSScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
+
+    // Public test 4
+    try {
+      input = "tests/fcfs4.input";
+      output = "tests/fcfs4.output";
+      scheduler = new FCFSScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
+
+    // Test SRTFScheduler
+    // Public test 1
+    try {
+      input = "tests/srtf1.input";
+      output = "tests/srtf1.output";
+      scheduler = new SRTFScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
+
+    // Public test 2
+    try {
+      input = "tests/srtf2.input";
+      output = "tests/srtf2.output";
+      scheduler = new SRTFScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
+
+    // Public test 3
+    try {
+      input = "tests/srtf3.input";
+      output = "tests/srtf3.output";
+      scheduler = new SRTFScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
+
+    // Public test 4
+    try {
+      input = "tests/srtf4.input";
+      output = "tests/srtf4.output";
+      scheduler = new SRTFScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
+
+    // Public test 5
+    try {
+      input = "tests/srtf5.input";
+      output = "tests/srtf5.output";
+      scheduler = new SRTFScheduler();
+      scheduler.schedule(input, output);
+    } catch (Exception e) {
+      // No exception should occur.
+    }
 
     // Test PSScheduler
   }
