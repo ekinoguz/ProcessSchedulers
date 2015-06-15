fe110c480ed9edd2d6bfa55982a95081e844ee87
ekinoguz <ekinoguz@gmail.com>
2015-06-15 16:37:49 -0700

    Make Tester more object oriented friendly.

--- Tester.java
+++ Tester.java
@@ -46,71 +46,47 @@ public class Tester {
     // Test FCFSScheduler
     scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs1.input", "p-tests/fcfs1.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs2.input", "p-tests/fcfs2.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs3.input", "p-tests/fcfs3.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs4.input", "p-tests/fcfs4.output");
 
     // Test SRTFScheduler
     scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf1.input", "p-tests/srtf1.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf2.input", "p-tests/srtf2.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf3.input", "p-tests/srtf3.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf4.input", "p-tests/srtf4.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf5.input", "p-tests/srtf5.output");
 
     // Test PSScheduler
     scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps1.input", "p-tests/ps1.output");
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps2.input", "p-tests/ps2.output");
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps3.input", "p-tests/ps3.output");
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps4.input", "p-tests/ps4.output");
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps5.input", "p-tests/ps5.output");
 
     // Private Tests
     scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs100.input", "p-tests/fcfs100.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs101.input", "p-tests/fcfs101.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs200.input", "p-tests/fcfs200.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs900.input", "p-tests/fcfs900.output");
-    scheduler = new FCFSScheduler();
     testScheduler(scheduler, "p-tests/fcfs901.input", "p-tests/fcfs901.output");
 
     scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf100.input", "p-tests/srtf100.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf101.input", "p-tests/srtf101.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf200.input", "p-tests/srtf200.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf900.input", "p-tests/srtf900.output");
-    scheduler = new SRTFScheduler();
     testScheduler(scheduler, "p-tests/srtf901.input", "p-tests/srtf901.output");
 
     scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps100.input", "p-tests/ps100.output"); // 100 random process, share 1
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps101.input", "p-tests/ps101.output"); // 9999 super long process, share 3
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps102.input", "p-tests/ps102.output"); // 4999 super long process, share 77
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps200.input", "p-tests/ps200.output"); // 2 super long process
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps900.input", "p-tests/ps900.output"); // Super long process ids
-    scheduler = new PSScheduler();
     testScheduler(scheduler, "p-tests/ps901.input", "p-tests/ps901.output"); // Shuffled super long process ids
   }
 }
